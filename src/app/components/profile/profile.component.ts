b22be390fd0d9daa9f41d51ea9713bb1a626a02b
Riley Thompson <rileythompson99@gmail.com>
2022-07-05 18:44:21 -0400

    router navigate instead of ngoninit

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -170,7 +170,7 @@ export class ProfileComponent implements OnInit {
 		var formData = new FormData();
 		formData.append('file', file);
 		this.users.ChangePicture(formData).subscribe(
-			res => this.ngOnInit(), 
+			res => this.router.navigateByUrl('profile'), 
 			err => alert(`Error changing picture: ${err.error}`)
 		)
 	}
